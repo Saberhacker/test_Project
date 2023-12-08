e9b607d2a671389d73008cfece97769cac285871
saber-seerat <saber.seerat66@gmail.com>
2023-12-08 16:03:05 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const res = await fetch("https://reqres.in/api/users?page=1");
+        const data = await res.json();
+        console.log(data.data);
+        setData(data.data)
+      }catch(err){
+        console.log(err);
+      }
+    };
+    fetchData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((item)=>{
+          return (
+            <div>
+              <p>{item.first_name + " " + item.last_name}</p>
+            </div>
+          )
+        })
+    }
+       
     </div>
   );
 }
